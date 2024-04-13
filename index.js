e38d85325b2c590a4484c44743da0734f92b702e
Siva Teja Arla <143586637+SivaTejaArla@users.noreply.github.com>
2024-04-13 07:42:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var typed = new Typed("#element", {
+  strings: [
+    "Full Stack Web developer",
+    "Android developer",
+    "UI/UX Designer",
+    "Graphic Designer",
+    "Full Stack Web Developer",
+  ],
+  typeSpeed: 110,
+});
+function openResume() {
+  window.open(
+    "https://drive.google.com/file/d/1-1FF73jmReghGc0CsoWtLH-eZ3LbJjPR/view?usp=sharing",
+    "_blank"
+  );
+}
+function openGitHub() {
+  window.open("https://github.com/SivaTejaArla", "_blank");
+}
+function openLinkedin() {
+  window.open("https://www.linkedin.com/in/siva-teja-arla/", "_blank");
+}
